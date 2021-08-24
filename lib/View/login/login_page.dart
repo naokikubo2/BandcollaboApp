ab4b909a0c523335aa93d0af53e937b9a52f37fe
naokikubo2 <naoki16871@gmail.com>
2021-08-24 22:27:29 +0900

    e-mail login design #2

--- lib/View/login/login_page.dart
+++ lib/View/login/login_page.dart
@@ -7,8 +7,10 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
+
           padding: EdgeInsets.all(24),
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
               TextFormField(
                 decoration: InputDecoration(labelText: 'メールアドレス'),
@@ -16,10 +18,27 @@ class LoginPage extends StatelessWidget {
               TextFormField(
                 decoration: InputDecoration(labelText: 'パスワード'),
               ),
+              const SizedBox(height: 8),
               Container(
-                padding: EdgeInsets.all(8),
+                width: double.infinity,
                 // メッセージ表示
-                child: Text("infoText"),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "新規登録"
+                  ),
+                )
+              ),
+              const SizedBox(height: 8),
+              Container(
+                  width: double.infinity,
+                  // メッセージ表示
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                        "ログイン"
+                    ),
+                  )
               ),
             ],
           ),
